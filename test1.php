ba633ba1cfeab6198af268aff5d84cb18fefcb81
Sorajak Phankong <art@golder.org>
2023-09-07 21:00:37 +0700

    ทดลองใช้ git

---
+++ test1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testการใช้gitบนVscode</title>
+</head>
+<body>
+    <p>ทดลองใช้gitบนvscode</p>
+</body>
+</html>
